d4d5b42bb4d9be2f5f87a46bc0fbfff8f810f2c8
ThachTuyenNura <thachtuyennura@gmail.com>
2025-10-02 18:18:16 +0700

    thêm giao diện Search

---
+++ src/components/allscreens/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    Button,
+    StyleSheet
+} from 'react-native';
+
+const Search = (props) => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default Search;
+
+const styles = StyleSheet.create({
+
+})

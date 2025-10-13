6fd404ca9880acb03cbc7b78ec5700a512cdc348
ThachTuyenNura <thachtuyennura@gmail.com>
2025-10-13 10:36:45 +0700

    update home

---
+++ src/components/allscreens/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Image, Text } from 'react-native';
+
+const Home = (props) => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default Home;
